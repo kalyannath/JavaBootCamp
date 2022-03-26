e0afd90250dc58e7a4d04d5176883bd7a88dfbe6
kalyannath <kalyannath82@gmail.com>
2022-03-26 17:34:25 +0530

    added question 25 elements less than 5 in a list

--- src/com/company/Q25ElementsLessThan5.java
+++ src/com/company/Q25ElementsLessThan5.java
@@ -3,34 +3,23 @@ package com.company;
 25.write a program that prints out all the elements of the list that are less than 5.
  */
 
+import java.util.ArrayList;
+
 public class Q25ElementsLessThan5 {
-    public static float[] lessThan5(float ...a) {
-        int len = 0;
-        for (float element: a)
+    public static ArrayList<Integer> lessThan5(int ...a) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int element: a)
         {
             if (element < 5)
             {
-                len++;
-            }
-        }
-        float[] result = new float[len];
-        int count = 0;
-        for (float element: a)
-        {
-            if (element < 5)
-            {
-                result[count] = element;
-                count++;
+                if (!result.contains(element)) {
+                    result.add(element);
+                }
             }
         }
         return result;
     }
     public static void main(String[] args) {
-        float[] ar1 = lessThan5(1, 3, 6, 7, 8, 3, 9);
-        System.out.println(ar1.length);
-        for (float element: ar1)
-        {
-            System.out.println(element);
-        }
+        System.out.println(lessThan5(1, 3, 6, 7, 8, 3, 9));
     }
 }
